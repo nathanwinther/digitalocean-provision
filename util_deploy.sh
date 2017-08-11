e2b08014fc99cbd8c6bdf0306b66610c5c95bd9d
Nathan Winther <nwinther@taunton.com>
2017-08-11 10:27:01 -0400

    update

--- util_deploy.sh
+++ util_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ansible-playbook --ask-vault-pass --user=root --private-key="private/digitalocean_rsa" --inventory-file="util_hosts.ini" --extra-vars="@util_vars_digitalocean.yml" util_playbook.yml
+ansible-playbook --user=root --private-key="private/digitalocean_rsa" --inventory-file="util_hosts.ini" --extra-vars="@util_vars_digitalocean.yml" util_playbook.yml
 
